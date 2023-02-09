24c5752835a27900d42b4e370ab63cef89ed8a5b
hgy <1359059613@qq.com>
2023-02-09 21:54:19 +0800

    创建新的界面

---
+++ VOIP/settingView.swift
@@ -0,0 +1,20 @@
+//
+//  settingView.swift
+//  VOIP
+//
+//  Created by 侯冠宇 on 2023/2/9.
+//
+
+import SwiftUI
+
+struct settingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct settingView_Previews: PreviewProvider {
+    static var previews: some View {
+        settingView()
+    }
+}
