fd25deb21e6ff74f1af6b17829896a0214b76c36
hgy <1359059613@qq.com>
2023-02-19 15:49:53 +0800

    新建文件

---
+++ VOIP/helloView.swift
@@ -0,0 +1,20 @@
+//
+//  helloView.swift
+//  VOIP
+//
+//  Created by 侯冠宇 on 2023/2/19.
+//
+
+import SwiftUI
+
+struct helloView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct helloView_Previews: PreviewProvider {
+    static var previews: some View {
+        helloView()
+    }
+}
